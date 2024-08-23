5d2fcf578f885dcae838f1a0507bb5bd8e83ad00
Théo Fargeas <theofargeas@MacBook-Pro-de-Theo.local>
2024-08-23 22:17:51 +0200

    [main]: Add native wind

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-red-600 font-bold text-xl">Hello NativeWind!</Text>
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
